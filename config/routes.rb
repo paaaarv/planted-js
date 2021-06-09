0e52ea7401694c6c79a1f4e31dec3effce6685c8
paaaarv <pkumar@trinity.edu>
2021-06-09 16:26:56 -0500

    added routes for water and light

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  
+
   resources :plants
+  resources :light, only: [:index]
+  resources :water, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
