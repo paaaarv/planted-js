ce78340f09ea6639e44a25a2d2345871bd2ae698
paaaarv <pkumar@trinity.edu>
2021-06-10 16:28:47 -0500

    added class for plant to create instances of objects

---
+++ frontend/index.js
@@ -0,0 +1,37 @@
+
+
+const schedule=
+    document.getElementById("schedule").addEventListener("click", function(){
+        fetch("http://localhost:3000/plants").then(function(response) {
+            return response.json().then(function(json){
+                for(let i=0; i<json.data.length; i++){
+                    let info =json.data[i]
+                    info = {
+                        name:info.attributes.name,
+                        water_id: info.relationships.light.data.id,
+                        light_id: info.relationships.water.data.id}
+                        let plant= new Plant(info);
+                debugger
+                plant.row();
+                console.log(plant)
+            }
+            });
+})
+    }
+
+    )
+
+class Plant{
+
+    constructor(info){
+        this.name= info.name;
+        this.water_id= info.water_id;
+        this.light_id= info.light_id;
+    }
+
+    row(){
+        
+        debugger
+    }
+
+}
